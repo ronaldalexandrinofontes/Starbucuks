04345ae13035b99aa8cb003759e540b6c5a509e0
ronaldalexandrinofontes <alexandrino937@gmail.com>
2025-05-11 18:48:18 -0300

    Starbucuks

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+/* function especifica onde quer trocar a cor*/
+/* document para chamar 1 pasta no html */
+
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".starbucks")
+
+function trocarCor(cor){
+
+    circulo.style.background = cor }
+
+function trocarImagem(img){
+    imagem.src = img
+}
+
+
